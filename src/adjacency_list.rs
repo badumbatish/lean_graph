f1ce28c1193796cfec7937ea6b1e8b55039033e1
jjasmine <tanghocle456@gmail.com>
2024-06-30 23:12:28 -0500

    Oh thank god it's working

--- src/adjacency_list.rs
+++ src/adjacency_list.rs
@@ -1,22 +1,38 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    hash::Hash,
+};
 
 use crate::storage::GraphStorage;
+use crate::storage::VertexError;
 
 pub struct AdjacencyList<V, E> {
     adjacency_list: HashMap<V, HashSet<(V, E)>>,
 }
 
-impl<V, E> GraphStorage<V, E> for AdjacencyList<V, E> {
+impl<V, E> GraphStorage<V, E> for AdjacencyList<V, E>
+where
+    V: Eq + Hash + Clone,
+    E: Eq + Hash + Clone,
+{
     fn new() -> Self {
-        todo!()
+        AdjacencyList {
+            adjacency_list: Default::default(),
+        }
     }
 
     fn add_vertex(&mut self, vertex: V) {
-        todo!()
+        self.adjacency_list
+            .entry(vertex.clone())
+            .or_insert(Default::default());
     }
 
     fn add_edge(&mut self, from: V, to: V, edge: E) {
-        todo!()
+        self.add_vertex(from.clone());
+        self.adjacency_list
+            .get_mut(&from)
+            .unwrap()
+            .insert((to, edge));
     }
 
     fn remove_edge(&mut self, from: &V, to: &V, edge: E) {
@@ -28,6 +44,14 @@ impl<V, E> GraphStorage<V, E> for AdjacencyList<V, E> {
     }
 
     fn neighbors(&self, vertex: &V) -> Vec<&V> {
+        todo!()
+    }
+
+    fn vertices(&self) -> Vec<&V> {
+        todo!()
+    }
+
+    fn edges(&self) -> Vec<&E> {
         todo!()
     }
 }
--- src/storage.rs
+++ src/storage.rs
@@ -1,8 +1,18 @@
-pub trait GraphStorage<V, E> {
+use std::hash::Hash;
+pub enum VertexError {
+    VertexExists,
+}
+pub trait GraphStorage<V, E>
+where
+    V: Eq + Hash + Clone,
+    E: Eq + Hash + Clone,
+{
     fn new() -> Self;
     fn add_vertex(&mut self, vertex: V);
     fn add_edge(&mut self, from: V, to: V, edge: E);
     fn remove_edge(&mut self, from: &V, to: &V, edge: E);
     fn has_edge(&self, from: &V, to: &V, edge: E) -> bool;
     fn neighbors(&self, vertex: &V) -> Vec<&V>;
+    fn vertices(&self) -> Vec<&V>;
+    fn edges(&self) -> Vec<&E>;
 }
